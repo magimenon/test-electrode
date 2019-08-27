9a530fe5c231d8d33c8bc92862e54a9fa5efa5f0
Mahesh Menon <mahesh.menon@cdk.com>
2019-08-27 00:04:45 -0700

    first commit

---
+++ archetype/config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "options": {
+    "criticalCSS": false,
+    "jest": true,
+    "mocha": false,
+    "typescript": false,
+    "sinon": false,
+    "eslint": true,
+    "postcss": false
+  },
+  "webpack": {
+    "cssModuleSupport": false
+  }
+}
